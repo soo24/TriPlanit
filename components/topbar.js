0fa8d2261224b0b2e63e709705126de5d53bb36a
soo <soo@sooui-MacBookPro.local>
2022-08-05 17:08:58 +0900

    상단바 초안1

---
+++ components/topbar.js
@@ -0,0 +1,63 @@
+import Link from 'next/link';
+import Image from 'next/image'
+import Logo from '../public/logo.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCalendarDays, faLocationDot, faUsersLine } from "@fortawesome/free-solid-svg-icons";
+
+export default function TopBar() {
+  return (
+    <div className='bg-blue-200 pt-6 pl-6w-full'>
+        <div className="grid grid-cols-12  grid-rows-2 ml-5 zf:grid-cols-4 ">
+
+          <div className='col-start-1 col-span-2'>
+            <Image src={Logo} alt="logo" height={30} width={150}/>
+          </div>
+
+          <form className="col-start-3 col-span-3 w-fit justify-items-center "> 
+            <input type="text" id="search" name="search" required minlength="1"
+            placeholder=" 🔎 검색"/>
+          </form>
+
+          <div className="col-start-11 zf:col-start-3 text-center"> 
+            <button> 
+              로그인
+            </button>
+          </div>
+
+          <div className="col-start-12 rounded-[5px] bg-white text-center w-9/12 mb-5 zf:hidden"> 
+            <button  > 
+              회원가입
+            </button>
+          </div>
+
+          <div className='col-start-1 col-span-2 text-center hover:cursor-pointer'>
+            <Link href="">
+              <div>
+                <FontAwesomeIcon icon={faCalendarDays} size='lg' className='mr-2'/>
+              여행 등록
+              </div>
+            </Link>
+          </div>
+
+          <div className='col-start-3 col-span-2 gap-4 text-center hover:cursor-pointer'>
+            <Link href="">
+            <div>
+                <FontAwesomeIcon icon={faLocationDot} size='lg' className='mr-2' />
+                여행지 추천
+              </div>
+            </Link>
+          </div>
+          <div className='col-start-5 gap-4 col-span-2 text-center hover:cursor-pointer'>
+            <Link href="">
+              <div>
+                <FontAwesomeIcon icon={faUsersLine} size='lg'className='mr-2' />
+               커뮤니티
+              </div>
+            </Link>
+          </div>
+
+        </div>
+    </div>
+   
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+    listStyleType: {
+      none: 'none',
+      disc: 'disc',
+      decimal: 'decimal',
+      square: 'square',
+      roman: 'upper-roman',
+    },
+    screens: {
+      'zf' : {'max': '639px'},
+
+      'sm': {'min': '640px', 'max': '767px'},
+      // => @media (min-width: 640px and max-width: 767px) { ... }
+
+      'md': {'min': '768px', 'max': '1023px'},
+      // => @media (min-width: 768px and max-width: 1023px) { ... }
+
+      'lg': {'min': '1024px', 'max': '1279px'},
+      // => @media (min-width: 1024px and max-width: 1279px) { ... }
+
+      'xl': {'min': '1280px', 'max': '1535px'},
+      // => @media (min-width: 1280px and max-width: 1535px) { ... }
+
+      '2xl': {'min': '1536px'},
+      // => @media (min-width: 1536px) { ... }
+    },
+    
+  },
+  plugins: [],
+}
