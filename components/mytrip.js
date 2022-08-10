48ba5a6d3843ed20e2ad8765a1bc5d375f68acb5
jiina28 <jiina_28@naver.com>
2022-08-10 17:45:25 +0900

    메인페이지 버튼 nav 연결

--- components/mytrip.js
+++ components/mytrip.js
@@ -1,8 +1,10 @@
 import Image from 'next/image'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlane } from '@fortawesome/free-solid-svg-icons';
+import { useRouter } from 'next/router';
 
 export default function mytrip() {
+    const router = useRouter();
     return (
         <div>
             <div className='grid grid-cols-2 gap-8 m-10'>
@@ -14,8 +16,10 @@ export default function mytrip() {
                 />
                 <div className='bg-blue-50 rounded-xl text-center h-full'>
                     <p className='text-2xl my-12 h-1/3'><FontAwesomeIcon icon={faPlane} color='rgb(37 99 235)' /> 나만의 여행을 만들어보세요</p>
-                    <button class="px-4 py-1 text-sm my-12 w-2/3 text-blue-600 font-semibold rounded-full border border-blue-200 hover:text-white hover:bg-blue-600 hover:border-transparent">
-                        일정 만들기</button>
+                    <button class="px-4 py-1 text-sm my-12 w-2/3 text-blue-600 font-semibold rounded-full border border-blue-200 hover:text-white hover:bg-blue-600 hover:border-transparent"
+                        onClick={() => router.push('/travelRegister')}
+                        >
+                        여행 만들기</button>
                 </div>
             </div>
         </div>
