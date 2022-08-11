558b09b1670a15adde07392154e6da7754fd5b8e
soo <soo@sooui-MacBookPro.local>
2022-08-11 11:19:01 +0900

    fixed error

--- pages/travelRegister.js
+++ pages/travelRegister.js
@@ -1,7 +1,9 @@
 import SetPlan from '../components/setPlan'
 import Head from 'next/head'
+import Mapscreen from '../components/Mapscreen';
+import PlanDay from '../components/plan_day'
 
-export default function travelRegister() {
+export default function TravelRegister() {
     return (
         <>
             <div>
@@ -11,12 +13,22 @@ export default function travelRegister() {
             </div>
             {/* 여기 div 스타일은 진아껄로 맞추기 */}
             <div>
+            {/* 오버레이 짜야함 */}
+                {/* <Mapscreen /> */}
                 <SetPlan />
+                
+                
+
             </div>
+            <div className='container w-[400px] bg-slate-50'>
+            <PlanDay/>
+           
+        </div>
 
             {/* 여기부터 수연코드 시작 */}
 
 
         </>
     );
+
 }
