b93aa5cb90d9a72ad3659223fa97bd74ff5dd6bd
JeongHyunHeo <jeonghyunheo619@gmail.com>
2022-08-16 08:48:57 +0900

    database schema created

--- backend/triplanit/models.py
+++ backend/triplanit/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class UserList(models.Model):
+    name = models.CharField(max_length=200)
+    date_birth = models.DateField()
+    gender = 
+    liked_plan
+
+class PlanList(models.Model):
+    owner = models.CharField(max_length=200)
+    plan_created_date = models.DateTimeField().auto 
+    startDate = models.DateTimeField()
+    endDate = models.DateTimeField()
+    title = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+    category
+    day
+
+class DayEvent(models.Model):
+    place = models.CharField(max_length=200)
+    time = models.DateTimeField()
+    detailDescription = models.CharField(max_length=200)
+    lng = models.FloatField()
+    lat = models.FloatField()
