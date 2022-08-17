f3dbfac883952fe325962249656ab271e0525a25
Juwon Hong <juwon2024@gmail.com>
2022-08-17 13:59:23 +0900

    deletion modal completed

--- components/plan_list.js
+++ components/plan_list.js
@@ -63,14 +63,14 @@ export default function Plan_list() {
                                     <CardModal
                                         title={"정말로 삭제하시겠습니까?"}
                                         content={"일정을 삭제하면 다시 복구할 수 없습니다."}
-                                        onConfirm={() => console.log('Delete Confirmed')}
-                                        onCancel={() => console.log('Delete Canceled')}
+                                        onConfirm={() => handleRemove(1)}
+                                        onCancel={() => console.log('Deletion Canceled')}
                                         buttons={[
-                                            { role: "cancel", onClick: () => console.log("custom test"), toClose: true, classes: "bg-zinc-500/20 px-4 py-2 rounded-lg hover:bg-zinc-500/30 transition-all duration-200", label: "취소" },
-                                            { role: "confirm", toClose: false, classes: "bg-blue-200 px-4 py-2 rounded-lg hover:bg-blue-400 transition-all duration-200", label: "삭제" }
+                                            { role: "cancel", toClose: true, classes: "bg-zinc-500/20 px-4 py-2 rounded-lg hover:bg-zinc-500/30 transition-all duration-200", label: "취소" },
+                                            { role: "confirm", toClose: true, classes: "bg-blue-200 px-4 py-2 rounded-lg hover:bg-blue-400 transition-all duration-200", label: "삭제" }
                                         ]}
                                         >
-                                        <FontAwesomeIcon icon={faTrashAlt} size="xs" onClick={handleRemove} /> 
+                                        <FontAwesomeIcon icon={faTrashAlt} size="xs"/> 
                                         </CardModal> </button></div>
                             </div>
 
