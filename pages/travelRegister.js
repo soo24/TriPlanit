459634a1432d834438f4d850ccc85575cb09ca63
jiina28 <jiina_28@naver.com>
2022-08-17 10:16:14 +0900

    nothing

--- pages/travelRegister.js
+++ pages/travelRegister.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Plan_sidebar from '../components/plan_sidebar'
+import Mapscreen from '../components/MapScreen';
 
 export default function TravelRegister() {
 
@@ -11,16 +12,16 @@ export default function TravelRegister() {
                 </Head>
             </div>
 
-            <div className='fixed'>
+            
                 {/* 오버레이 짜야함 */}
-                {/* <Mapscreen /> */}
-            </div>
+                <Mapscreen />
+            
  
-            <div> <Plan_sidebar /> </div>
+            {/* <div className='z-20'> <Plan_sidebar /> </div>
             
             <button className="fixed z-90 bottom-10 right-8 items-center px-6 py-2 bg-blue-400 hover:bg-blue-700 text-white font-xl rounded-md mx-2 hover:bg-blue-700 hover:drop-shadow-2xl hover:animate-bounce duration-300">
                 플랜 저장
-            </button>
+            </button> */}
 
         </>
     );
