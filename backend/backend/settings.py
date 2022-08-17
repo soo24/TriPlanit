2a395b2770288c0e80f7097eb0ee8aab5e5b5d1c
JeongHyunHeo <jeonghyunheo619@gmail.com>
2022-08-17 11:20:26 +0900

    connecting django with postgresql - checked connected

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -76,22 +76,22 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 # DATABASES = {
 #     'default': {
-#         'ENGINE' : 'django.db.backends.postgresql',
-#         'NAME' : 'backend',
-#         'USER' : 'jeonghyunheotimheo',
-#         'PASSWORD' : 'JH0619jh',
-#         'HOST' : 'localhost',
-#         'PORT' : '',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres', 
+        'USER': 'postgres', 
+        'PASSWORD': '1q2g3m4l',
+        'HOST': 'localhost', 
+        'PORT': '5432',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
